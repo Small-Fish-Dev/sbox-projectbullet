c2a11573e0e53069cfbe79367b5daf867b00cf99
lotuspar <91263142+lotuspar@users.noreply.github.com>
2022-12-07 06:29:58 +0800

    core/node: impl `WeaponNodeEntity`

---
+++ code/Core/Node/WeaponNodeEntity.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ProjectBullet.Core.Node.Description;
+using Sandbox;
+
+namespace ProjectBullet.Core.Node;
+
+public abstract class WeaponNodeEntity : Entity
+{
+	private class Connector
+	{
+		public WeaponNodeEntity WeaponNodeEntity = null;
+		public readonly ConnectorAttribute ConnectorAttribute;
+		public string Identifier => ConnectorAttribute.Identifier;
+
+		public Connector( ConnectorAttribute nextAttribute )
+		{
+			ConnectorAttribute = nextAttribute;
+		}
+	}
+
+	private readonly List<Connector> _connectors = new();
+
+	/// <summary>
+	/// Run <see cref="Execute"/> on connected node with provided connector ID
+	/// </summary>
+	/// <param name="identifier">Connector ID</param>
+	/// <param name="target">Target to pass to Execute (if any)</param>
+	/// <param name="point">Position to pass to Execute (if any)</param>
+	/// <exception cref="Exception"></exception>
+	protected void ExecuteConnector( string identifier, Entity target, Vector3 point )
+	{
+		foreach ( var connector in _connectors.Where( connector => connector.Identifier == identifier ) )
+		{
+			connector.WeaponNodeEntity?.Execute( target, point );
+			return;
+		}
+
+		// todo: write custom exception
+		throw new Exception( $"Unknown connector {identifier}" );
+	}
+
+	/// <summary>
+	/// Method to be executed when this node runs.
+	/// Either target or point should be non-null. *both don't have to be non-null!*
+	/// </summary>
+	/// <param name="target">Target (if any) entity hit</param>
+	/// <param name="point">Position (if any) of hit</param>
+	protected abstract void Execute( Entity target, Vector3 point );
+
+	public WeaponNodeDescription Description { get; }
+
+	protected WeaponNodeEntity()
+	{
+		Transmit = TransmitType.Owner;
+		Description = WeaponNodeDescription.Get( GetType() );
+
+		foreach ( var nextAttribute in Description.ConnectorAttributes )
+		{
+			// todo: _connectors could be incorrect after a hotload. don't do this here maybe?
+			_connectors.Add( new Connector( nextAttribute ) );
+		}
+	}
+}
