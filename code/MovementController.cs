de408383d092232c51310b6f6959d76619461226
lotuspar <91263142+lotuspar@users.noreply.github.com>
2022-12-13 07:00:14 +0800

    code: initial work on `MovementController`

---
+++ code/MovementController.cs
@@ -0,0 +1,45 @@
+﻿using Sandbox;
+
+namespace ProjectBullet;
+
+public partial class MovementController
+{
+	public enum FrictionLevels
+	{
+		Normal,
+		Skate,
+		Sticky,
+		Floating
+	}
+
+	private BasePlayer BasePlayer;
+
+	public MovementController( BasePlayer basePlayer ) => BasePlayer = basePlayer;
+
+	/// <summary>
+	/// Called when player dies / etc
+	/// </summary>
+	public virtual void Reset()
+	{
+	}
+
+	/// <summary>
+	/// Called on Simulate when player is alive
+	/// </summary>
+	public virtual void Simulate()
+	{
+		Start();
+
+		End();
+	}
+
+	/// <summary>
+	/// Called on FrameSimulate when player is alive
+	/// </summary>
+	public virtual void FrameSimulate()
+	{
+		Start();
+		
+		End();
+	}
+}
